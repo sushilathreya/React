ce38bd2d7fca8290605341aefafe8cba3653dbe0
Sushil Athreya <sushilathreya@gmail.com>
2021-07-02 13:24:03 +0530

    Studied React.memo, useMemo, useCallback, state scheduling and batching

---
+++ optimizaton-techniques/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useCallback } from "react";
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
+
+import "./App.css";
+
+function App() {
+  const [showPara, setShowPara] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  const toggleParaHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowPara((prevShowPara) => !prevShowPara);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showPara} />
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
+      <Button onClick={toggleParaHandler}>Toggle Para</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ optimizaton-techniques/src/components/Demo/DemoOutput.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DemoOutput(props) {
+    return <p>{props.show ? 'This is new!' : ''}</p>
+}
+
+export default React.memo(DemoOutput);
