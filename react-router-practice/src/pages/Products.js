66553a5516cc00e7f08b2f355ed504dc81f698b5
Sushil Athreya <sushilathreya@gmail.com>
2021-08-12 13:18:46 +0530

    Added project to learn basics of react router

---
+++ react-router-practice/src/pages/Products.js
@@ -0,0 +1,14 @@
+function Products() {
+  return (
+    <section>
+      <h1>The Products Page!</h1>
+      <ul>
+          <li>A Book</li>
+          <li>A Carpet</li>
+          <li>Online Course</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Products;
